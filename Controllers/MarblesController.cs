f54ecb0359a6608566e3dbc6040459defb67fa49
Brandyn Sullins <woofenwauzer@gmail.com>
2017-09-05 14:26:26 -0400

    intial commit

---
+++ Controllers/MarblesController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MarblesWithAPI.Models;
+
+namespace MarblesWithAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class MarblesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<MarbleModel> Get()
+        {
+            var Marble = new MarbleModel();
+            IEnumerable<MarbleModel> marbles = Marble.MarbleBagGetter();
+            return marbles;    
+        }
+
+         [HttpGet("/api/random")]
+        public MarbleModel GetRandom()
+        {
+            Console.WriteLine("not that");
+            
+            var Marble = new MarbleModel();
+            List<MarbleModel> marbles = Marble.MarbleBagGetter();
+
+            Random rnd = new Random();
+            Marble = marbles[rnd.Next(0, marbles.Count())];
+            return Marble;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public MarbleModel Get(int id)
+        {   
+            var Marble = new MarbleModel();
+            List<MarbleModel> marbles = Marble.MarbleBagGetter();
+            return marbles[id-1];
+        }
+
+       
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]int id, string color)
+        {
+            var Marble = new MarbleModel();
+            List<MarbleModel> marbles = Marble.MarbleBagGetter();
+            var newMarble = new MarbleModel{
+                ID = id,
+                MarbleColor = color
+            };
+            marbles.Add(newMarble);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Models/MarbleModel.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+
+namespace MarblesWithAPI.Models
+{
+    public class MarbleModel
+    {
+        public int ID { get; set; }
+        public string MarbleColor { get; set; }
+        public MarbleModel(){}
+        public List<MarbleModel> MarbleBagGetter()
+        {
+            var MarbleList = new List<MarbleModel>();
+            var Marble1 = new MarbleModel{
+                ID = 1,
+                MarbleColor = "Blue"
+            };
+            var Marble2 = new MarbleModel{
+                ID = 2,
+                MarbleColor = "Red"
+            };
+            var Marble3 = new MarbleModel{
+                ID = 3,
+                MarbleColor = "Green"
+            };
+            var Marble4 = new MarbleModel{
+                ID = 4,
+                MarbleColor = "Yellow"
+            };
+            MarbleList.Add(Marble1);
+            MarbleList.Add(Marble2);
+            MarbleList.Add(Marble3);
+            MarbleList.Add(Marble4);
+            return MarbleList;
+        }
+    }
+}
